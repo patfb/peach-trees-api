66aa79efa2a89a8d1908dde3789d2e3f027358f3
patfb <snailmail>
2019-11-17 11:17:59 -0500

    added express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
-
 const { readAllData, readFilteredData, updateUser } = require("../mongo/users");
+const { check, validationResult } = require("express-validator");
 
-/* GET users */
 router.get("/", async (req, res, next) => {
   const { firstName } = req.query;
   console.log("first name: ", firstName);
@@ -21,16 +20,34 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    if (req.body) {
-      // await createUser(req.body);
-      res.send(`added ${JSON.stringify(req.body)}`);
+router.post(
+  "/",
+  [
+    check("first")
+      .isAlpha()
+      .isLength({ min: 1 })
+      .withMessage("first must be at least 1 character long"),
+    check("last")
+      .isAlpha()
+      .isLength({ min: 1 })
+      .withMessage("last must be at least 1 character long")
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  } catch (err) {
-    next(err);
+
+    try {
+      if (req.body) {
+        // await createUser(req.body);
+        res.send(`added ${JSON.stringify(req.body)}`);
+      }
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 router.put("/", async (req, res, next) => {
   try {
